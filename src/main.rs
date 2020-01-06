8c569a3840d97777065d19de912c609c24ab5575
Fumiya Fujinaka <uzuna.kf@gmail.com>
2020-01-06 23:30:50 +0900

    add wordcount and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn count(input: impl BufRead) -> HashMap<String, usize> {
+    let re = Regex::new(r"\w+").unwrap();
+    let mut fregs = HashMap::new();
+
+    for line in input.lines() {
+        let line = line.unwrap();
+        for m in re.find_iter(&line) {
+            let word = m.as_str().to_string();
+            *fregs.entry(word).or_insert(0) += 1;
+        }
+    }
+    fregs
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect("1 argument FILENAME required");
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(&file);
+
+    let fregs = count(reader);
+    println!("{:?}", fregs);
+}
+
+#[test]
+fn read_testfile() {
+    let file = File::open("./src/tests/word.txt").unwrap();
+    let reader = BufReader::new(&file);
+
+    let fregs = count(reader);
+
+    assert_eq!(Some(&1), fregs.get("aa"));
+    assert_eq!(Some(&2), fregs.get("bb"));
+    assert_eq!(Some(&2), fregs.get("cc"));
+    assert_eq!(Some(&1), fregs.get("dd"));
 }
